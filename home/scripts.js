7106e63f190d0edcc223f0d3388dcaef2d6f9304
Ninjago Organization <86410849+PavanYadavGo@users.noreply.github.com>
2024-03-22 13:02:00 +0530

    good image

--- home/scripts.js
+++ home/scripts.js
@@ -3,6 +3,7 @@ function search() {
     fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
       .then(response => response.json())
       .then(data => {
+        fetchPokemonImage(data.species.url);
         displayPokemon(data);
       })
       .catch(error => {
@@ -15,10 +16,23 @@ function search() {
     const pokemonInfo = document.getElementById('pokemonInfo');
     pokemonInfo.innerHTML = `
       <h2>${pokemon.name}</h2>
-      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-      <p>#${pokemon.id}</p>
+      <div id="pokemonImage"></div>
+      <p>ID: ${pokemon.id}</p>
       <p>Height: ${pokemon.height}</p>
       <p>Weight: ${pokemon.weight}</p>
     `;
+  }
+  
+  function fetchPokemonImage(speciesUrl) {
+    fetch(speciesUrl)
+      .then(response => response.json())
+      .then(data => {
+        const imageUrl = data.sprites.other['official-artwork'].front_default;
+        const imageContainer = document.getElementById('pokemonImage');
+        imageContainer.innerHTML = `<img src="${imageUrl}" alt="Pokemon Image">`;
+      })
+      .catch(error => {
+        console.error('Error fetching Pokemon image:', error);
+      });
   }
   
