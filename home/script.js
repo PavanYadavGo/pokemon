e0fe953d5b8a2e619bd8c372bb961e6c309c7351
Ninjago Organization <86410849+PavanYadavGo@users.noreply.github.com>
2024-03-23 11:44:05 +0530

    optimizing time

--- home/script.js
+++ home/script.js
@@ -52,3 +52,23 @@ function displayPokemonList(pokemons) {
     `;
   });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = document.querySelectorAll('img.lazy');
+  if ("IntersectionObserver" in window) {
+    var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
